d79ee0c56d8b30962e40dfb1f0d321a02578e8c0
Tejomay Saha <tejomaysaha@outlook.com>
2024-01-30 14:23:52 +0530

    food ordering app created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,168 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
+import placeholder from "./placeholder.jpeg";
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <div>
+        <img className="logo" src={logo} alt="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
-}
+};
+
+//  image , name, ratings, cuisines
+const ResturantCard = ({ name, rating, cuisines, imgSrc }) => {
+  return (
+    <div className="rest-card">
+      <img src={imgSrc} alt="resturant img" />
+      <h3>{name}</h3>
+      <h4>Rating : {rating}</h4>
+      <h5>Cuisines : {cuisines.join()}</h5>
+    </div>
+  );
+};
+
+const resturants = [
+  {
+    name: "ABs",
+    rating: 4,
+    cuisines: ["Indian", "Chinese"],
+    imgSrc: placeholder,
+  },
+  {
+    name: "Berbeque",
+    rating: 4,
+    cuisines: ["Indian", "Chinese", "Continental"],
+    imgSrc: placeholder,
+  },
+  {
+    name: "Dominos",
+    rating: 4,
+    cuisines: ["Indian", "Italian"],
+    imgSrc: placeholder,
+  },
+  {
+    name: "Pizzahut",
+    rating: 4,
+    cuisines: ["Indian", "Chinese"],
+    imgSrc: placeholder,
+  },
+  {
+    name: "ABs",
+    rating: 4,
+    cuisines: ["Indian", "Chinese"],
+    imgSrc: placeholder,
+  },
+  {
+    name: "Berbeque",
+    rating: 4,
+    cuisines: ["Indian", "Chinese", "Continental"],
+    imgSrc: placeholder,
+  },
+  {
+    name: "Dominos",
+    rating: 4,
+    cuisines: ["Indian", "Italian"],
+    imgSrc: placeholder,
+  },
+  {
+    name: "Pizzahut",
+    rating: 4,
+    cuisines: ["Indian", "Chinese"],
+    imgSrc: placeholder,
+  },
+  {
+    name: "ABs",
+    rating: 4,
+    cuisines: ["Indian", "Chinese"],
+    imgSrc: placeholder,
+  },
+  {
+    name: "Berbeque",
+    rating: 4,
+    cuisines: ["Indian", "Chinese", "Continental"],
+    imgSrc: placeholder,
+  },
+  {
+    name: "Dominos",
+    rating: 4,
+    cuisines: ["Indian", "Italian"],
+    imgSrc: placeholder,
+  },
+  {
+    name: "Pizzahut",
+    rating: 4,
+    cuisines: ["Indian", "Chinese"],
+    imgSrc: placeholder,
+  },
+];
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search"></div>
+      <div className="rest-container">
+        {resturants.map((resturant, index) => (
+          <ResturantCard key={index} {...resturant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ul>
+        <li>Copyright @</li>
+        <li>Legal</li>
+        <li>Contact US</li>
+      </ul>
+    </footer>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
+
+/*
+//  Header
+      - logo
+      - nav
+          navitem
+
+// Body
+    - search
+      - Search Bar
+      - Button
+      - resturant container
+        - resturant cards
+            - Name
+            - Cuisine
+            - ratings
+
+// Footer
+    - Copyright
+    - links
+    - Address
+    - Contact
+*/
